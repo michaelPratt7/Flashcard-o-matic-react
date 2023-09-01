fffb078ab9b06aaf1e7fe40dde3dfe8eaf34d531
Michael Pratt <mrpratt7751@gmail.com>
2023-08-31 21:46:24 -0400

    updates

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -21,8 +21,8 @@ function Layout() {
           <Route path="/decks/new">
             <CreateDeck />
           </Route>
+          <NotFound />
           </Switch>
-        <NotFound path='*' />
       </div>
     </>
   );
