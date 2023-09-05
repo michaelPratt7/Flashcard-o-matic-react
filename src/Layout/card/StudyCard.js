b79ad2f4c377b6ae036ca2a29b4813891805921b
Michael Pratt <mrpratt7751@gmail.com>
2023-09-05 16:08:36 -0400

    updates

---
+++ src/Layout/card/StudyCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function StudyCard({deck}) {
+   return (
+    <div>{deck.cards}</div>
+    
+   )
+   
+    
+
+}
+
+export default StudyCard;
--- src/Layout/deck/StudyDeck.js
+++ src/Layout/deck/StudyDeck.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from "react";
 import {useParams} from "react-router-dom";
 import StudyBread from "../breadcrumbs/StudyBread";
 import {readDeck} from "../../utils/api";
+import StudyCard from "../card/StudyCard";
 
 function StudyDeck() {
    const [deck, setDeck] = useState([]);
@@ -18,6 +19,7 @@ return (
     <section>
         <StudyBread deck={deck} />
         <h1>Study: {deck.name}</h1>
+        <StudyCard deck={deck} />
         
     </section>
 )
